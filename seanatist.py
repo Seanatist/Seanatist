803e6c94f3a94f00437b0838a60568dbe76f2751
mayosholly <mayosholly@gmail.com>
2024-01-02 08:43:39 +0100

    latest

--- seanatist.py
+++ seanatist.py
@@ -1,1 +1,22 @@
 print("hello world")
+name = input("what is your name: ")
+print("hello", name)
+num1 = int(input("Enter num1: "))
+num2 = int(input("Enter num2: "))
+num3 = num1 * num2
+print(f"the product of {num1} and {num2} is {num3}. ")
+alpha = int(input("enter alpha:  "))
+beta = int(input("enter beta: "))
+omega = alpha * beta
+print(f"the product of {alpha} and {beta} is {omega}. ")
+x = float(input("Enter x: "))
+y = float(input("Enter y: "))
+z = x * y
+print(f" the product of {x} and {y} is {z}. ")
+f1 = 20
+if(f1>8):
+    print("f1 is good" )
+elif(f1<8):
+    print("f2 is not good" )
+else:
+    print("f2 is wonderful" )
